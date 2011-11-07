fdbc9958bdcc5cdc370f7e209796e43b7945b111
Mark Duffy <markaduffy@gmail.com>
2011-11-07 00:39:06 -0500

    initial commit

---
+++ views/helpers/wijmo.php
@@ -0,0 +1,57 @@
+<?php
+
+class WijmoHelper extends AppHelper {
+
+	var $helpers = array('Html');
+
+	function beforeRender() {
+		$this->Html->css('/wijmo-open/themes/rocket/jquery-wijmo.css', null, array('inline' => false));
+		$this->Html->css('/wijmo-open/css/jquery.wijmo-open.1.4.1.css', null, array('inline' => false));
+	    $this->Html->script('http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js',false);
+		$this->Html->script('https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/jquery-ui.min.js',false);
+		$this->Html->script('/wijmo-open/js/jquery.wijmo-open.1.4.1.min.js',false);
+	}
+
+
+	function tooltip($properties) {
+		
+		extract($properties);
+
+		if (!isset($title)) $title = null;
+		if (!isset($class)) $class = "wijmocake-tooltip";
+		if (!isset($tip)) $tip = "Default ToolTip";
+		if (!isset($tag)) $tag = "span";
+		if (!isset($url)) $url = "#";
+
+		$this->generate_script($class);
+
+		if ($tag == 'a'){
+			return $this->Html->tag($tag, $title, array('href' => $url, 'class' => $class, 'title' => $tip));
+		} else {
+			return $this->Html->tag($tag, $title, array('class' => $class, 'title' => $tip));
+		}
+
+	}
+
+	function generate_script($class){
+		
+		ob_start();
+
+		?>
+
+				jQuery(function($) {
+					$('.<?php echo $class ?>').wijtooltip();
+				});
+
+		<?php
+
+		$jscript = ob_get_contents();
+		ob_end_clean();
+
+		echo $this->Html->scriptBlock($jscript, array("inline" => false));
+
+	}
+
+}
+
+?>
